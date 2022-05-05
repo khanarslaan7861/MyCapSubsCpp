4d38e8c20cda53717b5523092b16149748988cb1
khanarslaan7861 <49835460+khanarslaan7861@users.noreply.github.com>
2022-05-05 22:22:42 +0530

    Created size.cpp
    C++ Program to Find Size of int, float, double and char

---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Size of Char: "<<sizeof(char)<<" byte"<<endl;
+	cout<<"Size of Int: "<<sizeof(int)<<" byte"<<endl;
+	cout<<"Size of Float: "<<sizeof(float)<<" byte"<<endl;
+	cout<<"Size of Double: "<<sizeof(double)<<" byte"<<endl;
+	return 0;
+}
