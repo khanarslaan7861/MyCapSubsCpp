9613625916e4e6b9139be21e9bc197c34d0f61c3
khanarslaan7861 <49835460+khanarslaan7861@users.noreply.github.com>
2022-05-05 22:32:39 +0530

    Created prod.cpp
    C++ Program to find the product of two numbers entered by the User

---
+++ prod.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	float a, b, c;
+	cout<<"Enter the first number: ";
+	cin>>a;
+	cout<<"Enter the second number: ";
+	cin>>b;
+	c = a * b;
+	cout<<"The product of "<<a<<" and "<<b<<" is "<<c;
+	getch();
+	return 0;
+}
